593e1d9fafd22838aefea177a513bf98d03c2a3e
Shah Khurram <khurramgaur8@gmail.com>
2024-01-01 22:51:26 +0530

    this is for me

--- shahkhurram/example.php
+++ shahkhurram/example.php
@@ -4,18 +4,7 @@ $data2 =1;
 $data3 =$data2." ".$data;
 
 echo $data3;
-echo $data3;
-echo $data3;
-echo $data3;
-echo $data3;
-echo $data3;
-echo $data3;
-echo $data3;
-echo $data3;
-echo $data3;
-echo $data3;
-echo $data3;
-echo $data3;
+
 
 
 ?>
